b915c82211ef7e8dbc1ba9e96da72f5bfd0f8cad
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-11 03:59:13 +0000

    Adding tarfile member sanitization to extractall()

--- coop/search.py
+++ coop/search.py
@@ -50,7 +50,29 @@ def main(log_dir_or_file, split):
         # Extract archive
         tempdir = tempfile.mkdtemp()
         with tarfile.open(log_dir_or_file, "r:gz") as archive:
-            archive.extractall(tempdir)
+            
+            import os
+            
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(archive, tempdir)
         log_dir = Path(tempdir)
     else:
         log_dir = Path(log_dir_or_file)
